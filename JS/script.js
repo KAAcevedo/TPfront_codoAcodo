70483ffefde8907fe1c8d33b0587f8107a4d8b08
Karina A. Acevedo <acevedokarinaalejandra@gmail.com>
2022-06-05 08:12:44 -0300

    main

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+let email = document.getElementById('correo_electronico')
+
+if (email.includes("@")){
+    console.log("Es un e-mail VÁLIDO.")
+} else {
+    console.log("Es un e-mail INVÁLIDO.")
+}
+
