dda0284b4859452baa6fb0c08e5b1d2557dabf26
Karina A. Acevedo <acevedokarinaalejandra@gmail.com>
2022-06-05 13:59:55 -0300

    Eliminacion del Js

--- JS/script.js
+++
@@ -1,8 +0,0 @@
-let email = document.getElementById('correo_electronico')
-
-if (email.includes("@")){
-    console.log("Es un e-mail VÁLIDO.")
-} else {
-    console.log("Es un e-mail INVÁLIDO.")
-}
-
